68aecbf3104b24d502c24fbf70a6067bdc938613
ursopanda <gagallo7@gmail.com>
2014-10-09 11:09:19 +1100

    created test.java servlet for testing the view home.jsp - home page style.css - CSS stylesheet web.xml modified because of the creation of servlet
    libs for jsp and servlet added

---
+++ Assignment2/src/edu/unsw/comp9321/test.java
@@ -0,0 +1,50 @@
+package edu.unsw.comp9321;
+
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class test
+ */
+@WebServlet("/test")
+public class test extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public test() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//		ServletOutputStream out = response.getOutputStream();
+//		out.println("<html><body><h1>No data found! PLEASE CHECK THE XML FILE!</h1></body></html>");
+		
+		String nextPage = "home.jsp";
+		
+		RequestDispatcher rd = request.getRequestDispatcher("/" + nextPage);
+		rd.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
