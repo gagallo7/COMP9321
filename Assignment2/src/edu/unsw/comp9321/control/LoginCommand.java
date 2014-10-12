8e8c1f3e12bf8dff7bade3f4cecc5489cf023a51
ursopanda <gagallo7@gmail.com>
2014-10-12 14:56:15 +1100

    Revert "merge test.java e final controller"
    This reverts commit 5e0acfcb03ccda9d930fe44e617174b28b953335.

--- Assignment2/src/edu/unsw/comp9321/control/LoginCommand.java
+++ Assignment2/src/edu/unsw/comp9321/control/LoginCommand.java
@@ -2,33 +2,16 @@ package edu.unsw.comp9321.control;
 
 import java.io.IOException;
 
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 public class LoginCommand implements Command {
 
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		String username = request.getParameter("username");
-		
-		logger.info("Username = " + username);
-		if (username != null) {
-			HttpSession session = request.getSession();
-			if (username.equals("admin")) {
-				session.setAttribute("UserRole", "manager");
-			} else {
-				session.setAttribute("UserRole", "user");
-			}
-			session.setAttribute("username", username);
-		}
-
-		RequestDispatcher rd = request.getRequestDispatcher("/home.jsp");
-		rd.forward(request, response);
+		// TODO Auto-generated method stub
 
 	}
 
